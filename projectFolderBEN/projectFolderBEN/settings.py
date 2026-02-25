6d5fead8926eebcc228473288144b10f3c5f87a8
Rafsun Ahmad <71074703+Rafsun001@users.noreply.github.com>
2026-02-25 11:19:26 +0600

    Clear email credentials in settings.py
    Removed sensitive email credentials from settings.py for smtp

--- projectFolderBEN/projectFolderBEN/settings.py
+++ projectFolderBEN/projectFolderBEN/settings.py
@@ -181,8 +181,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "ahmadrafsun001@gmail.com"
-EMAIL_HOST_PASSWORD = "yeqk dqkw jazr qecj" 
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = "" 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 REST_FRAMEWORK = {
